968aa0b8177801f205124ec33477a09130921c1e
toadkicker <todd@toadkicker.com>
2021-07-07 06:43:10 -0700

    fix type name

--- amplified_todo/lib/models/UpdatedTodo.dart
+++ amplified_todo/lib/models/UpdatedTodo.dart
@@ -18,11 +18,10 @@
 import 'package:amplify_datastore_plugin_interface/amplify_datastore_plugin_interface.dart';
 import 'package:flutter/foundation.dart';
 
-
-/** This is an auto generated class representing the UpdatedTodo type in your schema. */
+/** This is an auto generated class representing the Todo type in your schema. */
 @immutable
-class UpdatedTodo extends Model {
-  static const classType = const _UpdatedTodoModelType();
+class Todo extends Model {
+  static const classType = const _TodoModelType();
   final String id;
   final String? _name;
   final String? _description;
@@ -30,137 +29,156 @@ class UpdatedTodo extends Model {
 
   @override
   getInstanceType() => classType;
-  
+
   @override
   String getId() {
     return id;
   }
-  
+
   String get name {
     try {
       return _name!;
-    } catch(e) {
-      throw new DataStoreException(DataStoreExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage, recoverySuggestion: DataStoreExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion, underlyingException: e.toString());
+    } catch (e) {
+      throw new DataStoreException(
+          DataStoreExceptionMessages
+              .codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion: DataStoreExceptionMessages
+              .codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString());
     }
   }
-  
+
   String? get description {
     return _description;
   }
-  
+
   bool get isComplete {
     try {
       return _isComplete!;
-    } catch(e) {
-      throw new DataStoreException(DataStoreExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage, recoverySuggestion: DataStoreExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion, underlyingException: e.toString());
+    } catch (e) {
+      throw new DataStoreException(
+          DataStoreExceptionMessages
+              .codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion: DataStoreExceptionMessages
+              .codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString());
     }
   }
-  
-  const UpdatedTodo._internal({required this.id, required name, description, required isComplete}): _name = name, _description = description, _isComplete = isComplete;
-  
-  factory UpdatedTodo({String? id, required String name, String? description, required bool isComplete}) {
-    return UpdatedTodo._internal(
-      id: id == null ? UUID.getUUID() : id,
-      name: name,
-      description: description,
-      isComplete: isComplete);
+
+  const Todo._internal(
+      {required this.id, required name, description, required isComplete})
+      : _name = name,
+        _description = description,
+        _isComplete = isComplete;
+
+  factory Todo(
+      {String? id,
+      required String name,
+      String? description,
+      required bool isComplete}) {
+    return Todo._internal(
+        id: id == null ? UUID.getUUID() : id,
+        name: name,
+        description: description,
+        isComplete: isComplete);
   }
-  
+
   bool equals(Object other) {
     return this == other;
   }
-  
+
   @override
   bool operator ==(Object other) {
     if (identical(other, this)) return true;
-    return other is UpdatedTodo &&
-      id == other.id &&
-      _name == other._name &&
-      _description == other._description &&
-      _isComplete == other._isComplete;
+    return other is Todo &&
+        id == other.id &&
+        _name == other._name &&
+        _description == other._description &&
+        _isComplete == other._isComplete;
   }
-  
+
   @override
   int get hashCode => toString().hashCode;
-  
+
   @override
   String toString() {
     var buffer = new StringBuffer();
-    
-    buffer.write("UpdatedTodo {");
+
+    buffer.write("Todo {");
     buffer.write("id=" + "$id" + ", ");
     buffer.write("name=" + "$_name" + ", ");
     buffer.write("description=" + "$_description" + ", ");
-    buffer.write("isComplete=" + (_isComplete != null ? _isComplete!.toString() : "null"));
+    buffer.write("isComplete=" +
+        (_isComplete != null ? _isComplete!.toString() : "null"));
     buffer.write("}");
-    
+
     return buffer.toString();
   }
-  
-  UpdatedTodo copyWith({String? id, String? name, String? description, bool? isComplete}) {
-    return UpdatedTodo(
-      id: id ?? this.id,
-      name: name ?? this.name,
-      description: description ?? this.description,
-      isComplete: isComplete ?? this.isComplete);
+
+  Todo copyWith(
+      {String? id, String? name, String? description, bool? isComplete}) {
+    return Todo(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        description: description ?? this.description,
+        isComplete: isComplete ?? this.isComplete);
   }
-  
-  UpdatedTodo.fromJson(Map<String, dynamic> json)  
-    : id = json['id'],
-      _name = json['name'],
-      _description = json['description'],
-      _isComplete = json['isComplete'];
-  
+
+  Todo.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        _name = json['name'],
+        _description = json['description'],
+        _isComplete = json['isComplete'];
+
   Map<String, dynamic> toJson() => {
-    'id': id, 'name': _name, 'description': _description, 'isComplete': _isComplete
-  };
+        'id': id,
+        'name': _name,
+        'description': _description,
+        'isComplete': _isComplete
+      };
 
   static final QueryField ID = QueryField(fieldName: "updatedTodo.id");
   static final QueryField NAME = QueryField(fieldName: "name");
   static final QueryField DESCRIPTION = QueryField(fieldName: "description");
   static final QueryField ISCOMPLETE = QueryField(fieldName: "isComplete");
-  static var schema = Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
-    modelSchemaDefinition.name = "UpdatedTodo";
-    modelSchemaDefinition.pluralName = "UpdatedTodos";
-    
+  static var schema =
+      Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
+    modelSchemaDefinition.name = "Todo";
+    modelSchemaDefinition.pluralName = "Todos";
+
     modelSchemaDefinition.authRules = [
-      AuthRule(
-        authStrategy: AuthStrategy.PUBLIC,
-        operations: [
-          ModelOperation.CREATE,
-          ModelOperation.UPDATE,
-          ModelOperation.DELETE,
-          ModelOperation.READ
-        ])
+      AuthRule(authStrategy: AuthStrategy.PUBLIC, operations: [
+        ModelOperation.CREATE,
+        ModelOperation.UPDATE,
+        ModelOperation.DELETE,
+        ModelOperation.READ
+      ])
     ];
-    
+
     modelSchemaDefinition.addField(ModelFieldDefinition.id());
-    
+
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-      key: UpdatedTodo.NAME,
-      isRequired: true,
-      ofType: ModelFieldType(ModelFieldTypeEnum.string)
-    ));
-    
+        key: Todo.NAME,
+        isRequired: true,
+        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
+
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-      key: UpdatedTodo.DESCRIPTION,
-      isRequired: false,
-      ofType: ModelFieldType(ModelFieldTypeEnum.string)
-    ));
-    
+        key: Todo.DESCRIPTION,
+        isRequired: false,
+        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
+
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-      key: UpdatedTodo.ISCOMPLETE,
-      isRequired: true,
-      ofType: ModelFieldType(ModelFieldTypeEnum.bool)
-    ));
+        key: Todo.ISCOMPLETE,
+        isRequired: true,
+        ofType: ModelFieldType(ModelFieldTypeEnum.bool)));
   });
 }
 
-class _UpdatedTodoModelType extends ModelType<UpdatedTodo> {
-  const _UpdatedTodoModelType();
-  
+class _TodoModelType extends ModelType<Todo> {
+  const _TodoModelType();
+
   @override
-  UpdatedTodo fromJson(Map<String, dynamic> jsonData) {
-    return UpdatedTodo.fromJson(jsonData);
+  Todo fromJson(Map<String, dynamic> jsonData) {
+    return Todo.fromJson(jsonData);
   }
 }
